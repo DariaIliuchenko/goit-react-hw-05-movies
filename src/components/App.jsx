dcf8b5ffc6064e3e8bcae9d767ef94e17975a061
DariaIliuchenko <102221418+DariaIliuchenko@users.noreply.github.com>
2022-10-03 13:43:12 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
 import { lazy } from 'react';
 
-const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
-const MoviesPage = lazy(() => import("../pages/MoviesPage/MoviesPage"));
-const MovieDetailsPage = lazy(() => import("../pages/MovieDetailsPage/MovieDetailsPage"));
-const Cast = lazy(() => import("../pages/Cast/Cast"));
-const Reviews = lazy(() => import("../pages/Reviews/Reviews"));
-const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage'));
+const Cast = lazy(() => import('../pages/Cast/Cast'));
+const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'))
 
 export const App = () => {
   return (
@@ -16,14 +16,13 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="movies" element={<MoviesPage />} />
-          <Route path="movies/:movieId" element={<MovieDetailsPage/> } >
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-        </Route>        
+        </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
-    
 };
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
 return (
-    <h1>Page not found. Weclome to <Link to='/'>home page</Link></h1>
+    <h1>Page not found. Welcome to <Link to='/'>Home page</Link></h1>
 )}
 
 export default NotFoundPage
